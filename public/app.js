6132e1a860ff855a8b5ee3a60ef59e8dc61216fe
Anna Goldberg <annalexgoldberg@gmail.com>
2015-07-10 11:07:54 -0400

    Added cheat filter and ng-include html

--- public/app.js
+++ public/app.js
@@ -1,4 +1,12 @@
 var app = angular.module('flashCards', []);
 
 
+app.filter('cheat', function(){
+    return function(collection){
+        return collection.filter(function(answer){
+            return answer.correct === true;
+        })
+    };
+})
+
 
